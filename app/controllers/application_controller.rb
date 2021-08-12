836013c5ec72a9aaaa9725364a7e766847a364f0
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-12 16:23:14 -0700

    fell down a rabbit hole. still no error handling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
+  #.find raises an exception. This rescues us from that exception
+  #with the method defined below
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
 
   def render_not_found_response(exception)
     render json: { error: exception.message }, status: :not_found
   end
+
+  #we can follow the same logic for a 422 error
+  # (validation error)
 
 end
