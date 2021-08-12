3403922b606d04fe599edc8e3b3c5169866fed1e
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-12 11:25:47 -0700

    added destroy action

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,10 +1,16 @@
 class SightingsController < ApplicationController
+  #so that I can see all the created sightings
   def index
     sightings = Sighting.all
     render json: sightings
   end
   
-  
+
+  def show
+    sighting = Sighting.find(params[:id])
+    render json: sighting
+  end
+  #I feel like there's a better way to do this but I'll need to look a bit more.
   def create
     sighting = Sighting.new(sighting_params)
     if sighting.save
@@ -13,6 +19,13 @@ class SightingsController < ApplicationController
       render json: sighting.errors.full_messages
     end
   end
+
+  def destroy
+    sighting = Sighting.find(params[:id])
+    sighting.destroy
+    head :no_content
+  end
+
 
 
   private
