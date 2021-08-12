e8ac472d7eb99cc80f223efae8f9b16c673b5099
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-12 16:06:54 -0700

    error handling for 'not found'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+
+  def render_not_found_response(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
 end
