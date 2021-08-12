c5f2114eb930783c9260cb5eb49ee60dfa6e5469
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-12 15:10:57 -0700

    added animal checks

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,6 @@
 class Animal < ApplicationRecord
   has_many :sightings
+
+  validates :common_name, presence: true
+  validates :latin_name, presence: true
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'is valid with valid attributes' do
+  test_animal = Animal.create({
+      common_name: "Banana Slug",
+      latin_name: "Ariolimax californicus",
+      animal_class: "gastropod"
+  })
+    expect(test_animal).to be_valid
+  end
+
+  it 'is invalid without a common name' do
+    test_animal = Animal.create({
+      latin_name: "Ariolimax californicus",
+      animal_class: "gastropod"
+    })
+
+    expect(test_animal.errors[:common_name]).to_not be_empty
+
+  end
+
+
+  it 'is invalid without a latin name' do
+    test_animal = Animal.create({
+      common_name: "Banana Slug",
+      animal_class: "gastropod"
+    })
+
+    expect(test_animal.errors[:latin_name]).to_not be_empty
+
+  end
+
+
+  # it 'is invalid without a latin name' do
+  #   test_animal = Animal.create({
+  #     common_name: "Banana Slug",
+  #     animal_class: "gastropod"
+  #   })
+
+  #   expect(test_animal.errors[:latin_name]).to_not be_empty
+
+  # end
+
+
+
+
 end
