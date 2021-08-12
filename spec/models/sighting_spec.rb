796981f2100ca52efffc3cd694765020c29bf875
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-12 14:44:02 -0700

    test for valid sighting

--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Sighting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do 
+    test_animal = Animal.create({
+      common_name: "Banana Slug",
+      latin_name: "Ariolimax californicus",
+      animal_class: "gastropod"
+    })
+    test_sighting = test_animal.sightings.create(date: Date.today,latitude: 0, longitude: 0) 
+    expect(test_sighting).to be_valid
+  end
 end
