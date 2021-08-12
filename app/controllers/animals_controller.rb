392aa33eea5af580580e5404dc279d926ad0d77d
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-12 10:21:48 -0700

    added index and show actions to controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,26 @@
 class AnimalsController < ApplicationController
+  def index
+    animals = Animal.all
+    render json: animals
+  end
+
+  def show
+    animal = Animal.find(params[:id])
+    render json: animal
+  end
+
+
+  def create
+  end
+
+  def update
+  end
+
+
+  def destroy
+  end
+
+
+  private
+
 end
