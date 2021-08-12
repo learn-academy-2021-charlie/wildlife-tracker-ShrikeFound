f3ef04620556f37bfdb3d306de9e887352e22a49
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-12 12:03:59 -0700

    added date range to sightings index

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,10 +1,18 @@
 class SightingsController < ApplicationController
   #so that I can see all the created sightings
   def index
-    sightings = Sighting.all
+    start_date = params[:start_date].to_date || Date.today.at_beginning_of_month
+    end_date   = params[:end_date].to_date ||  Date.today.at_beginning_of_month.next_month
+    
+    sightings = Sighting.where(date: start_date..end_date)
     render json: sightings
   end
   
+  ##index method from example:
+  # def index
+  #   sightings = Sighting.where(date: params[:start_date]..params[:end_date])
+  #   render json: sightings
+  # end
 
   def show
     sighting = Sighting.find(params[:id])
