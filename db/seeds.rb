cd2a6b3a52de6e0c7d9695f48597149ad10f79ac
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-12 10:12:38 -0700

    added animal resource and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Animal.destroy_all
+
+animals = [
+  {
+    common_name: "Arctic Fox",
+    latin_name: "Alopex Vulpini",
+    animal_class: "mammal"
+  },
+  {
+    common_name: "Ladybird Beetle",
+    latin_name: "Coccinella magnifica",
+    animal_class: "insect"
+  },
+  {
+    common_name: "Banana Slug",
+    latin_name: "Ariolimax californicus",
+    animal_class: "gastropod"
+  },
+
+
+
+]
+
+animals.each do |animal|
+  Animal.create animal
+end
